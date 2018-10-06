3d1c76e188d3096d6e6ded7fc9037806ecaa70e5
Avner Khan <avnerkhan@Avners-MacBook-Pro.local>
2018-10-06 02:34:20 -0500

    Post requests in Jquery

--- src/static/scripts/script.js
+++ src/static/scripts/script.js
@@ -26,6 +26,10 @@ function progress(dir)
   if(dir == 'right' && prog < 4)
   {
     prog++;
+    if(prog == 1)
+    {
+      sendtest();
+    }
     if (prog == 0){
       $(".left").hide();
     }
@@ -41,8 +45,29 @@ function progress(dir)
   }
   console.log(prog);
   $("#progressbar").css("width", prog*25 + "%");
+}
 
-
-
+function sendtest()
+{
+  console.log("Running");
+  var senderName = document.getElementById("name").value;
+  var senderEmail = document.getElementById("email").value;
+  var url = "http://129.114.104.6:5000/submit"
+  var data = {
+    "user_id" : "new_user",
+    "data" : {
+      "user_first_name" : senderName,
+      "user_last_name" : "Khan"
+    }
+  }
+  $.ajax({
+  type: "POST",
+  url: url,
+  data: JSON.stringify(data),
+  success: function(data){
+    console.log(data);
+  },
+  contentType: "application/json"
+});
 
 }
