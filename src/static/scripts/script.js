6a34bf20667e5315d244ca9d7dde78a8a93f5d58
Avner Khan <avnerkhan@Avners-MacBook-Pro.local>
2018-10-06 05:28:39 -0500

    Updated frontend functionality

--- src/static/scripts/script.js
+++ src/static/scripts/script.js
@@ -10,7 +10,7 @@ function progress(dir)
   if(dir == 'left' && prog > 0)
   {
     prog--;
-    if (prog == 0){
+    if (prog <= 0){
       $(".left").hide();
     }
     else {
@@ -23,51 +23,124 @@ function progress(dir)
       $(".right").show();
     }
   }
-  if(dir == 'right' && prog < 4)
+  if(dir == 'right' && prog < 5)
   {
-    prog++;
-    if(prog == 1)
-    {
-      sendtest();
-    }
-    if (prog == 0){
-      $(".left").hide();
-    }
-    else {
-      $(".left").show();
-    }
-    if (prog == 4){
-      $(".right").hide();
-    }
-    else {
-      $(".right").show();
-    }
+    if(sendtest(prog)){
+      prog++;
+      if (prog == 0){
+        $(".left").hide();
+      }
+      else {
+        $(".left").show();
+      }
+      if (prog == 4){
+        $(".right").hide();
+      }
+      else {
+        $(".right").show();
+      }
+  }
   }
   console.log(prog);
   $("#progressbar").css("width", prog*25 + "%");
 }
 
-function sendtest()
+function sendtest(prog)
 {
-  console.log("Running");
-  var senderName = document.getElementById("name").value;
-  var senderEmail = document.getElementById("email").value;
   var url = "http://129.114.104.6:5000/submit"
-  var data = {
-    "user_id" : "new_user",
-    "data" : {
-      "user_first_name" : senderName,
-      "user_last_name" : "Khan"
+  if(prog == 0)
+  {
+    var firstParam = document.getElementById("first").value;
+    var secondParam = document.getElementById("second").value;
+    var thirdParam = document.getElementById("third").value;
+    if(firstParam == "" || secondParam == "" || thirdParam == "")
+    {
+      window.alert("You have not filled in the last form completely, go back and please fill it out!")
+      return false;
+    }
+    var dataNew = {
+      "user_id" : "new_user",
+      "data" : {
+        "user_first_name" : firstParam,
+        "user_last_name" : secondParam,
+        "user_middle_name" : thirdParam
+      }
     }
   }
+  else if(prog == 1) //change this to be take user id from here
+  {
+    var firstParam = document.getElementById("fourth").value;
+    var secondParam = document.getElementById("fifth").value;
+    var thirdParam = document.getElementById("sixth").value;
+    if(firstParam == "" || secondParam == "" || thirdParam == "")
+    {
+      window.alert("You have not filled in the last form completely, go back and please fill it out!")
+      return false;
+    }
+    var dataNew = {
+      "user_id" : "new_user",  //actual id
+      "data" : {
+        "user_street" : firstParam,
+        "user_city" : secondParam,
+        "user_state" : thirdParam
+      }
+    }
+  }
+  else if(prog == 2)
+  {
+    var firstParam = document.getElementById("seventh").value;
+    var secondParam = document.getElementById("eigth").value;
+    var thirdParam = document.getElementById("ninth").value;
+    var fourthParam = document.getElementById("tenth").value;
+    var fifthParam = document.getElementById("eleventh").value;
+    if(firstParam == "" || secondParam == "" || thirdParam == "")
+    {
+      window.alert("You have not filled in the last form completely, go back and please fill it out!");
+      return false;
+    }
+    var dataNew = {
+      "user_id" : "new_user",  //actual id
+      "data" : {
+        "user_ssn" : firstParam,
+        "user_dob" : secondParam,
+        "user_email" : thirdParam,
+        "user_mobile_phone" : fourthParam,
+        "user_home_phone" : fifthParam
+      }
+    }
+  }
+  else if(prog == 3)
+  {
+    var firstParam = document.getElementById("twelvth").value;
+    var secondParam = document.getElementById("thirteenth").value;
+    var thirdParam = document.getElementById("fourteenth").value;
+    var fourthParam = document.getElementById("fifteenth").value;
+    if(firstParam == "" || secondParam == "" || thirdParam == "")
+    {
+      window.alert("You have not filled in the last form completely, go back and please fill it out!");
+      return false;
+    }
+    var dataNew = {
+      "user_id" : "new_user",  //actual id
+      "data" : {
+        "user_twitter" : firstParam,
+        "user_insta" : secondParam,
+        "user_linkedin" : thirdParam
+      }
+    }
+
+  }
+
+  console.log(dataNew);
+
   $.ajax({
   type: "POST",
   url: url,
-  data: JSON.stringify(data),
+  data: JSON.stringify(dataNew),
+  contentType: "application/json",
   success: function(data){
     console.log(data);
-  },
-  contentType: "application/json"
+  }
 });
 
 }
